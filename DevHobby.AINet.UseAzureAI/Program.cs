9294594b957074a12e03805f34ce44e39c71c662
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-06-23 11:24:47 +0200

    Add DevHobby.AINet.UseAzureAI

---
+++ DevHobby.AINet.UseAzureAI/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+
+
+
+Console.ReadLine();
